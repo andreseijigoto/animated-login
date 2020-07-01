c4b324193a93f3816d83705e5f6f26ee689b1927
André Seiji Goto <andreseijigoto@gmail.com>
2020-07-01 19:10:30 -0300

    fix(title): margin-bottom for title

--- react/src/views/login/index.tsx
+++ react/src/views/login/index.tsx
@@ -1,6 +1,4 @@
 import React, { useState, MouseEvent } from 'react';
-// import { Link } from 'react-router-dom';
-// import { FiLogIn } from 'react-icons/fi';
 import './styles.scss';
 
 const Login = () => {
